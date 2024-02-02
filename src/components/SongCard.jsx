b07575e892bfd466196997c561646bc4a78d914d
Sagar Tiwari <sagartiwari5030@gmail.com>
2024-02-03 01:03:21 +0530

    push2

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { FaHeart } from 'react-icons/fa'; 
+import { useState } from 'react';
 
 import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
@@ -16,7 +18,12 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true));
   };
+  const [clicked, setClicked] = useState(false);
 
+  const handleLikeClick = () => {
+    setClicked((prevClicked) => !prevClicked);
+    // setIsLiked(!isLiked);
+  };
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
@@ -28,9 +35,11 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
             handlePause={handlePauseClick}
             handlePlay={handlePlayClick}
           />
+          
         </div>
-        <div className="text-left flex-end">hello</div>
+        
         <img alt="song_img" src={song.thumbnail} className="w-full h-full rounded-lg" />
+        
       </div>
 
       <div className="mt-4 flex flex-col">
@@ -44,8 +53,15 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
             {song?.artist[0]?.name}
           </Link>
         </p>
+        
+      </div>
+      <div className="absolute bottom-0 right-0 p-4 text-white ">
+        <FaHeart   className={`mr-1 inline cursor-pointer ${clicked ? 'text-red-500 text-2xl' : ''}`}
+          onClick={handleLikeClick}/>
+        <span>{""}</span>
       </div>
     </div>
+    // {"`Liked by ${likedBy}`"}
   );
 };
 
