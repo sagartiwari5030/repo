3c610b966634ff67060e742355a67c11e383c16b
Sagar Tiwari <sagartiwari5030@gmail.com>
2024-02-09 23:52:10 +0530

    change

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -65,7 +65,7 @@ const Discover = () => {
           </select>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-5 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
           {loading && <Loader title="Loading songs..." />}
           {error && <Error />}
           {!loading &&
